a605669456aac2e4738e17d02e63c7c4e350cab3
Dave Hansen <dave.hansen@intel.com>
2015-05-11 10:48:22 -0700

    pkeys-5-arch_validate_prot

--- arch/x86/include/uapi/asm/mman.h
+++ arch/x86/include/uapi/asm/mman.h
@@ -8,4 +8,18 @@
 
 #include <asm-generic/mman.h>
 
+#ifndef arch_validate_prot
+/*
+ * This is called from mprotect().  PROT_GROWSDOWN and PROT_GROWSUP have
+ * already been masked out.
+ *
+ * Returns true if the prot flags are valid
+ */
+#define arch_validate_prot(prot) (\
+	(prot & ~(PROT_READ | PROT_WRITE | PROT_EXEC | PROT_SEM |	\
+	 PROT_PKEY0 | PROT_PKEY1 | PROT_PKEY2 | PROT_PKEY3)) == 0)	\
+
+#endif
+
+
 #endif /* _ASM_X86_MMAN_H */
