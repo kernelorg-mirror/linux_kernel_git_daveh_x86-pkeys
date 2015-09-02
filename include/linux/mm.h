21c522c9d8bd5692a79ce4ae079cc217cfa736b8
Dave Hansen <dave.hansen@intel.com>
2015-09-02 15:53:06 -0700

    x86, pkeys: arch-specific protection bits
    Lots of things seem to do:
    
            vma->vm_page_prot = vm_get_page_prot(flags);
    
    and the ptes get created right from things we pull out
    of ->vm_page_prot.  So it is very convenient if we can
    store the protection key in flags and vm_page_prot, just
    like the existing permission bits (_PAGE_RW/PRESENT).  It
    greatly reduces the amount of plumbing and arch-specific
    hacking we have to do in generic code.
    
    This also takes the new PROT_PKEY{0,1,2,3} flags and
    turns *those* in to VM_ flags for vma->vm_flags.
    
    The protection key values are stored in 4 places:
    	1. "prot" argument to system calls
    	2. vma->vm_flags, filled from the mmap "prot"
    	3. vma->vm_page prot, filled from vma->vm_flags
    	4. the PTE itself.
    
    The pseudocode for these for steps are as follows:
    
    	mmap(PROT_PKEY*)
    	vma->vm_flags 	  = ... | arch_calc_vm_prot_bits(mmap_prot);
    	vma->vm_page_prot = ... | arch_vm_get_page_prot(vma->vm_flags);
    	pte = pfn | vma->vm_page_prot
    
    Note that these are new definitions for x86:
    
    	arch_vm_get_page_prot()
    	arch_calc_vm_prot_bits()

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -163,6 +163,10 @@ extern unsigned int kobjsize(const void *objp);
 
 #if defined(CONFIG_X86)
 # define VM_PAT		VM_ARCH_1	/* PAT reserves whole VMA at once (x86) */
+# define VM_PKEY_BIT0	VM_HIGH_ARCH_0	/* A protection key is a 4-bit value */
+# define VM_PKEY_BIT1	VM_HIGH_ARCH_1
+# define VM_PKEY_BIT2	VM_HIGH_ARCH_2
+# define VM_PKEY_BIT3	VM_HIGH_ARCH_3
 #elif defined(CONFIG_PPC)
 # define VM_SAO		VM_ARCH_1	/* Strong Access Ordering (powerpc) */
 #elif defined(CONFIG_PARISC)
