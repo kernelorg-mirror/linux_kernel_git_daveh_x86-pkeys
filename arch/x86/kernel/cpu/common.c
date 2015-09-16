df2503ac78632edb9a30fa3c0427e43e9e9a33c1
Dave Hansen <dave.hansen@intel.com>
2015-09-16 09:46:27 -0700

    x86, pkeys: actually enable Memory Protection Keys in CPU
    This sets the bit in 'cr4' to actually enable the protection
    keys feature.  We also include a boot-time disable for the
    feature "nopku".
    
    Seting X86_CR4_PKE will cause the X86_FEATURE_OSPKE cpuid
    bit to appear set.  At this point in boot, identify_cpu()
    has already run the actual CPUID instructions and populated
    the "cpu features" structures.  We need to go back and
    re-run identify_cpu() to make sure it gets updated values.
    
    We *could* simply re-populate the 11th word of the cpuid
    data, but this is probably quick enough.
    
    Also note that with the cpu_has() check and X86_FEATURE_PKU
    present in disabled-features.h, we do not need an #ifdef
    for setup_pku().

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -289,6 +289,31 @@ static __always_inline void setup_smap(struct cpuinfo_x86 *c)
 }
 
 /*
+ * Protection Keys are not available in 32-bit mode.
+ */
+static __always_inline void setup_pku(struct cpuinfo_x86 *c)
+{
+	if (!cpu_has(c, X86_FEATURE_PKU))
+		return;
+
+	cr4_set_bits(X86_CR4_PKE);
+	/*
+	 * Seting X86_CR4_PKE will cause the X86_FEATURE_OSPKE
+	 * cpuid bit to be set.  We need to ensure that we
+	 * update that bit in this CPU's "cpu_info".
+	 */
+	get_cpu_cap(c);
+}
+#ifdef CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS
+static __init int setup_disable_pku(char *arg)
+{
+	setup_clear_cpu_cap(X86_FEATURE_PKU);
+	return 1;
+}
+__setup("nopku", setup_disable_pku);
+#endif /* CONFIG_X86_64 */
+
+/*
  * Some CPU features depend on higher CPUID levels, which may not always
  * be available due to CPUID level capping or broken virtualization
  * software.  Add those features to this table to auto-disable them.
@@ -947,6 +972,7 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 	init_hypervisor(c);
 	x86_init_rdrand(c);
 	x86_init_cache_qos(c);
+	setup_pku(c);
 
 	/*
 	 * Clear/Set all flags overriden by options, need do it
