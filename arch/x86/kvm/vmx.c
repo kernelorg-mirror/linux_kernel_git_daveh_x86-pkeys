0b8bfebb69e4e29d5f35e1fbc0479a58d7bf23df
Dave Hansen <dave.hansen@intel.com>
2015-10-09 10:06:04 -0700

    KVM,pkeys: Disable pkeys for guests in non-paging mode in  hardware
    Pkeys is disabled if CPU is in non-paging mode in hardware.
    However KVM always uses paging mode to emulate guest non-paging
    mode with TDP. To emulate this behavior, pkeys needs to be
    manually disabled when guest switches to non-paging mode.
    
    Signed-off-by: Huaitong Han <huaitong.han@intel.com>
    
    index 83b7b5c..434e670 100644

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -3645,14 +3645,14 @@ static int vmx_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4)
 			hw_cr4 &= ~X86_CR4_PAE;
 			hw_cr4 |= X86_CR4_PSE;
 			/*
-			 * SMEP/SMAP is disabled if CPU is in non-paging mode
-			 * in hardware. However KVM always uses paging mode to
-			 * emulate guest non-paging mode with TDP.
-			 * To emulate this behavior, SMEP/SMAP needs to be
+			 * SMEP/SMAP/PKU is disabled if CPU is in non-paging
+			 * mode in hardware. However KVM always uses paging
+			 * mode to emulate guest non-paging mode with TDP.
+			 * To emulate this behavior, SMEP/SMAP/PKU needs to be
 			 * manually disabled when guest switches to non-paging
 			 * mode.
 			 */
-			hw_cr4 &= ~(X86_CR4_SMEP | X86_CR4_SMAP);
+			hw_cr4 &= ~(X86_CR4_SMEP | X86_CR4_SMAP | X86_CR4_PKE);
 		} else if (!(cr4 & X86_CR4_PAE)) {
 			hw_cr4 &= ~X86_CR4_PAE;
 		}
