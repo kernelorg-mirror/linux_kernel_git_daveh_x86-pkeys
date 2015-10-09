9bed10d588cc8cfc2192f7297a16ce1017b9f1df
Dave Hansen <dave.hansen@intel.com>
2015-10-09 10:05:59 -0700

    x86: wire up mprotect_key() system call
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    This is all that we need to get the new system call itself
    working on x86.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: linux-api@vger.kernel.org

--- arch/x86/include/uapi/asm/mman.h
+++ arch/x86/include/uapi/asm/mman.h
@@ -27,6 +27,12 @@
 
 #define ARCH_VM_PKEY_FLAGS (VM_PKEY_BIT0 | VM_PKEY_BIT1 | VM_PKEY_BIT2 | VM_PKEY_BIT3)
 
+#define arch_calc_vm_prot_bits(prot, key) ( 		\
+		((key) & 0x1 ? VM_PKEY_BIT0 : 0) |      \
+		((key) & 0x2 ? VM_PKEY_BIT1 : 0) |      \
+		((key) & 0x4 ? VM_PKEY_BIT2 : 0) |      \
+		((key) & 0x8 ? VM_PKEY_BIT3 : 0))
+
 #endif /* CONFIG_X86_INTEL_MEMORY_PROTECTION_KEYS */
 
 #include <asm-generic/mman.h>
