db2829afcf7c1d98780125878acde80de5c4d67b
Dave Hansen <dave.hansen@intel.com>
2015-11-11 15:28:07 -0800

    mm: simplify get_user_pages() PTE bit handling
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    The current get_user_pages() code is a wee bit more complicated
    than it needs to be for pte bit checking.  Currently, it establishes
    a mask of required pte _PAGE_* bits and ensures that the pte it
    goes after has all those bits.
    
    This consolidates the three identical copies of this code.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>

--- arch/x86/mm/gup.c
+++ arch/x86/mm/gup.c
@@ -63,6 +63,21 @@ retry:
 #endif
 }
 
+static inline int pte_allows_gup(unsigned long entry_flags, int write)
+{
+	/*
+	 * 'entry_flags' can come from a pte, pmd or pud.  Make
+	 * sure to only check flags here that are valid *and* the
+	 * same value on all 3 types.  (PAT is currently the only
+	 * one where that is true and is not checked here).
+	 */
+	if (!(entry_flags & (_PAGE_PRESENT|_PAGE_USER)))
+		return 0;
+	if (write && !(entry_flags & (_PAGE_RW)))
+		return 0;
+	return 1;
+}
+
 /*
  * The performance critical leaf functions are made noinline otherwise gcc
  * inlines everything into a single function which results in too much
@@ -71,12 +86,7 @@ retry:
 static noinline int gup_pte_range(pmd_t pmd, unsigned long addr,
 		unsigned long end, int write, struct page **pages, int *nr)
 {
-	unsigned long mask;
 	pte_t *ptep;
-
-	mask = _PAGE_PRESENT|_PAGE_USER;
-	if (write)
-		mask |= _PAGE_RW;
 
 	ptep = pte_offset_map(&pmd, addr);
 	do {
@@ -88,8 +98,8 @@ static noinline int gup_pte_range(pmd_t pmd, unsigned long addr,
 			pte_unmap(ptep);
 			return 0;
 		}
-
-		if ((pte_flags(pte) & (mask | _PAGE_SPECIAL)) != mask) {
+		if (!pte_allows_gup(pte_flags(pte), write) ||
+		    pte_special(pte)) {
 			pte_unmap(ptep);
 			return 0;
 		}
@@ -117,14 +127,10 @@ static inline void get_head_page_multiple(struct page *page, int nr)
 static noinline int gup_huge_pmd(pmd_t pmd, unsigned long addr,
 		unsigned long end, int write, struct page **pages, int *nr)
 {
-	unsigned long mask;
 	struct page *head, *page;
 	int refs;
 
-	mask = _PAGE_PRESENT|_PAGE_USER;
-	if (write)
-		mask |= _PAGE_RW;
-	if ((pmd_flags(pmd) & mask) != mask)
+	if (!pte_allows_gup(pmd_flags(pmd), write))
 		return 0;
 	/* hugepages are never "special" */
 	VM_BUG_ON(pmd_flags(pmd) & _PAGE_SPECIAL);
@@ -193,14 +199,10 @@ static int gup_pmd_range(pud_t pud, unsigned long addr, unsigned long end,
 static noinline int gup_huge_pud(pud_t pud, unsigned long addr,
 		unsigned long end, int write, struct page **pages, int *nr)
 {
-	unsigned long mask;
 	struct page *head, *page;
 	int refs;
 
-	mask = _PAGE_PRESENT|_PAGE_USER;
-	if (write)
-		mask |= _PAGE_RW;
-	if ((pud_flags(pud) & mask) != mask)
+	if (!pte_allows_gup(pud_flags(pud), write))
 		return 0;
 	/* hugepages are never "special" */
 	VM_BUG_ON(pud_flags(pud) & _PAGE_SPECIAL);
