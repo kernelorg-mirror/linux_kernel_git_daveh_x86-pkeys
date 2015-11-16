7089ea4f0882ba3a8954267993e82c06f3df4cd9
Dave Hansen <dave.hansen@intel.com>
2015-11-16 12:36:30 -0800

    x86, pkeys: make mprotect_key() mask off additional vm_flags
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    Today, mprotect() takes 4 bits of data: PROT_READ/WRITE/EXEC/NONE.
    Three of those bits: READ/WRITE/EXEC get translated directly in to
    vma->vm_flags by calc_vm_prot_bits().  If a bit is unset in
    mprotect()'s 'prot' argument then it must be cleared in vma->vm_flags
    during the mprotect() call.
    
    We do the by first calculating the VMA flags we want set, then
    clearing the ones we do not want to inherit from the original VMA:
    
    	vm_flags = calc_vm_prot_bits(prot, key);
    	...
    	newflags = vm_flags;
    	newflags |= (vma->vm_flags & ~(VM_READ | VM_WRITE | VM_EXEC));
    
    However, we *also* want to mask off the original VMA's vm_flags in
    which we store the protection key.
    
    To do that, this patch adds a new macro:
    
    	ARCH_VM_FLAGS_AFFECTED_BY_MPROTECT
    
    which allows the architecture to specify additional bits that it would
    like cleared.  We use that to ensure that the VM_PKEY_BIT* bits get
    cleared.
    
    This got missed in my testing because I was always going from a pkey=0
    VMA to a nonzero one.  The current code works when we only set bits
    but never clear them.  I've fixed this up in my testing.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>

--- arch/x86/include/asm/pkeys.h
+++ arch/x86/include/asm/pkeys.h
@@ -5,6 +5,8 @@
 				CONFIG_NR_PROTECTION_KEYS : 1)
 #define arch_validate_pkey(pkey) (((pkey) >= 0) && ((pkey) < arch_max_pkey()))
 
+#define ARCH_VM_PKEY_FLAGS (VM_PKEY_BIT0 | VM_PKEY_BIT1 | VM_PKEY_BIT2 | VM_PKEY_BIT3)
+
 #endif /*_ASM_X86_PKEYS_H */
 
 
