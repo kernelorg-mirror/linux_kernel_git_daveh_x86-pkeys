6ffa49ecd5fa7ef6a9642d1003af9cb93a6e0680
Dave Hansen <dave.hansen@intel.com>
2015-12-03 14:13:15 -0800

    x86, pkeys: allocation/free syscalls
    From: Dave Hansen <dave.hansen@linux.intel.com>
    
    This patch adds two new system calls:
    
    	int pkey_alloc(unsigned long flags, unsigned long init_access_rights)
    	int pkey_free(int pkey);
    
    These establish which protection keys are valid for use by
    userspace.  A key which was not obtained by pkey_alloc() may not
    be passed to pkey_mprotect().
    
    In addition, the 'init_access_rights' argument to pkey_alloc() specifies
    the rights that will be established for the returned pkey.  For instance
    
    	pkey = pkey_alloc(flags, PKEY_DENY_WRITE);
    
    will return with the bits set in PKRU such that writing to 'pkey' is
    already denied.  This keeps userspace from needing to have knowledge
    about manipulating PKRU.  It is still free to do so if it wishes, but
    it is no longer required.
    
    The kernel does _not_ enforce that this interface must be used for
    changes to PKRU, even for keys it does not control.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: linux-api@vger.kernel.org

--- include/linux/pkeys.h
+++ include/linux/pkeys.h
@@ -23,6 +23,29 @@ static inline int vma_pkey(struct vm_area_struct *vma)
 {
 	return 0;
 }
+
+static inline bool mm_pkey_is_allocated(struct mm_struct *mm, int pkey)
+{
+	return (pkey == 0);
+}
+
+static inline int mm_pkey_alloc(struct mm_struct *mm)
+{
+	return -1;
+}
+
+static inline int mm_pkey_free(struct mm_struct *mm, int pkey)
+{
+	WARN_ONCE(1, "free of protection key when disabled");
+	return -EINVAL;
+}
+
+static inline int arch_set_user_pkey_access(struct task_struct *tsk, int pkey,
+			unsigned long init_val)
+{
+	return 0;
+}
+
 #endif /* ! CONFIG_ARCH_HAS_PKEYS */
 
 #endif /* _LINUX_PKEYS_H */
